082d9a907d1e359a872665bf62c239f7bb184895
lapu torres~ <lapu.torres@gmail.com>
2023-04-17 15:48:01 -0500

    agregadas las animaciones skills y about

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,70 @@
-/*Scroll navbar hidden */
+//ANIMATION
+const animationSkills = anime ({
+    targets: ".skills__list .card-top",
+    translateY: [200,0],
+    duration: 4000,
+    scale: [0.5,1],
+    borderRadius: ['50%', '10%'],
+    backgroundColor: '#313131',
+  easing: 'steps(2)',
+  delay: anime.stagger(1200),
+    autoplay: false,
+    });
+const animationSkillsBottom = anime ({
+    targets: ".card-bottom",
+    translateY: [200,0],
+    duration: 4000,
+    scale: [0.5,1],
+    borderRadius: ['50%', '10%'],
+    backgroundColor: '#313131',
+  easing: 'easeInOutQuad',
+  delay: anime.stagger(1200),
+    autoplay: false,
+    });
+const aboutLeft = anime ({
+    targets: ".about .title",
+    translateX: [-650,0],
+    duration: 4000,
+    scale: [0.5,1],    
+  easing: 'steps(10)',
+    autoplay: false,
+    });
+const aboutRight = anime ({
+    targets: ".about .text",
+    translateX: [650,0],
+    duration: 4000,
+    scale: [0.5,1],    
+  easing: 'steps(10)',
+    autoplay: false,
+    });
+    
 
-const body = document.body;
-const nav = document.querySelector(".page-header");
-const menu = document.querySelector(".page-header .navbar");
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-let lastScroll = 0;
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll == 0) {
-    nav.classList.remove(scrollUp);
-    return;
+//Anime Scroll 
+
+const skills = document.querySelector(".skills");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
+
+
+const animateOnScroll = function (div, speed = 100, offset = 300){
+  const scrollPorcent = window.pageYOffset - div.offsetTop;
+  return (scrollPorcent + offset) / speed;
+}
+
+//SCROLL LISTENER 
+window.onscroll = function () {
+  
+    
+    animationSkills.seek(animateOnScroll(skills, 200,200) * animationSkills.duration);
+    animationSkillsBottom.seek(animateOnScroll(skills, 200,-200) * animationSkillsBottom.duration);
+    aboutLeft.seek(animateOnScroll(about, 200,300) * aboutLeft.duration);
+    aboutRight.seek(animateOnScroll(about, 200,300) * aboutRight.duration);
+
+  
   }
 
-  if (currentScroll > lastScroll && !nav.classList.contains(scrollDown)) {
-    // down
-    nav.classList.remove(scrollUp);
-    nav.classList.add(scrollDown);
-  } else if (currentScroll < lastScroll && nav.classList.contains(scrollDown)) {
-    // up
-    nav.classList.remove(scrollDown);
-    nav.classList.add(scrollUp);
-  }
-  lastScroll = currentScroll;
-});
+     
+   
 
 //LOADER
 
@@ -43,5 +84,4 @@ particlesJS.load("particles-js", "assets/particles.json", function () {
 $("input").on("change", function () {
   $("body").toggleClass("blue");
 });
-
 
